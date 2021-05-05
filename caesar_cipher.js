d29d4bbddd4a5f4e4da21a9350d310fe056770ac
Olga Studzenichnik <okrypets@gmail.com>
2021-05-05 22:10:21 +0300

    feat: implement validation for cli

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -1,6 +1,9 @@
 import { pipeline } from 'stream';
 import { readStream, writeStream, transformStream} from "./src/streams.js";
 import { input, output, shift, action } from "./src/options.js";
+import { checkIsValid } from "./src/validation.js"
+
+checkIsValid({ input, output, shift, action });
 
 pipeline(readStream(input), transformStream(shift, action), writeStream(output), error => {
     if (error) {
