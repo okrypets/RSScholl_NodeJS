9318a7578dbb037b732a89dc1c57baf60898403b
Olga Studzenichnik <okrypets@gmail.com>
2021-05-08 17:51:09 +0300

    feat: add setting file for prettier and eslint

--- src/crypto.js
+++ src/crypto.js
@@ -3,9 +3,9 @@ if (action !== 'encode') shift *= -1;
 return text.split("").map(symbol => {
    if (symbol === '\r' ||  symbol === '\n') return symbol
     const charCode = symbol.charCodeAt(0);    
-    if(charCode >= 65 && charCode <=  90) {
+    if (charCode >= 65 && charCode <=  90) {
       return String.fromCharCode((charCode - 65 + +shift) % 26 + 65);
-     } else if(charCode >= 97 && charCode <= 122){
+     } else if (charCode >= 97 && charCode <= 122) {
       return String.fromCharCode((charCode - 97 + +shift) % 26 + 97);
      } else {
         return symbol;
