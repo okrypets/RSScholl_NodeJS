0acc4231edb00c1c0869a31984ddde5a60a4a1f8
Olga Studzenichnik <okrypets@gmail.com>
2021-05-08 18:59:49 +0300

    fix: incorrect coding so that encoded letters do not go beyond the Latin alphabet with negative shift

--- src/crypto.js
+++ src/crypto.js
@@ -2,15 +2,21 @@ export const crypto = (text, shift, action) => {
 if (action !== 'encode') shift *= -1;
 return text.split("").map(symbol => {
    if (symbol === '\r' ||  symbol === '\n') return symbol
-    const charCode = symbol.charCodeAt(0);    
+    const charCode = symbol.charCodeAt(0);
     if (charCode >= 65 && charCode <=  90) {
-      return String.fromCharCode((charCode - 65 + +shift) % 26 + 65);
+      const codedCharCode = (charCode - 65 + +shift) % 26 + 65;
+      let stringValue = String.fromCharCode(codedCharCode);
+      if (codedCharCode < 65 ) {
+         stringValue = String.fromCharCode(codedCharCode + 26)
+      }
+      return stringValue;
      } else if (charCode >= 97 && charCode <= 122) {
-        let charCodeValue = String.fromCharCode((charCode - 97 + +shift) % 26 + 97);
-        if (charCodeValue < 97) {
-            charCodeValue += 26
+        const codedCharCode = (charCode - 97 + +shift) % 26 + 97
+        let stringValue = String.fromCharCode(codedCharCode);
+        if (codedCharCode < 97) {
+         stringValue = String.fromCharCode(codedCharCode + 26)
         }
-      return charCodeValue;
+      return stringValue;
      } else {
         return symbol;
      }
